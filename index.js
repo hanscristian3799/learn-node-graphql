c14aaadb20b2b219f7049a84ad71503199fc3819
Hans Cristian <hans.cristian@binus.ac.id>
2022-05-27 17:56:57 +0700

    fisrt commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const PORT = 6969;
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./Shemas/index')
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(PORT, () => {
+    console.log("Server is running");
+})
