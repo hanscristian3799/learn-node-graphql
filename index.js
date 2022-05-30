e18bab44100ccfeb2038dd4f7fca0577dfbf24da
Hans Cristian <hans.cristian@binus.ac.id>
2022-05-30 17:56:40 +0700

    feat: add cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express();
+var cors = require('cors');
 const PORT = 6969;
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./Shemas/index')
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
